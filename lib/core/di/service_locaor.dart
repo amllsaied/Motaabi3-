799ff9c7588d350602aefa4620c7888832099da2
Aml <amlsaied2004@gmail.com>
2026-09-22 12:17:06 +0300

    Update app localization and add core network structure
    - Change app name to Arabic in AndroidManifest.xml and Info.plist
    - Update main.dart to reflect new app title
    - Add new dependencies in pubspec.yaml for network handling
    - Create initial structure for network-related files and validators
    - Add translation files for Arabic and English

---
+++ lib/core/di/service_locaor.dart
@@ -0,0 +1,16 @@
+
+
+// import 'package:dio/dio.dart';
+// import 'package:get_it/get_it.dart';
+
+
+// GetIt getIt = GetIt.instance;
+
+
+// Future<void> setupGetIt()async{
+
+//   getIt.registerLazySingleton<Dio>(()=>DioFactory.getDio());
+
+//   getIt.registerLazySingleton<RegisterRepo>(()=>RegisterRepo(getIt()));
+
+// }
